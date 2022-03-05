73e2ed73e55fde941c401b8c5f3e1a918fc34093
Uniqcoda <etokwudomary@gmail.com>
2022-03-05 19:40:15 +0100

    fix bug in firefox browser

--- colorGame.js
+++ colorGame.js
@@ -1,112 +1,108 @@
-let numCircles = 6;
+let numCircles = 3;
 let colors = [];
 let pickedColor;
-let circles = document.querySelectorAll(".circle");
-let colorDisplay = document.getElementById("colorDisplay");
-let messageDisplay = document.querySelector("#message");
-let h1 = document.querySelector("h1");
-let resetButton = document.querySelector("#reset");
-let modeButtons = document.querySelectorAll(".mode");
+let circles = document.querySelectorAll('.circle');
+let colorDisplay = document.getElementById('colorDisplay');
+let messageDisplay = document.querySelector('#message');
+let h1 = document.querySelector('h1');
+let resetButton = document.querySelector('#reset');
+let modeButtons = document.querySelectorAll('.mode');
 
-function init(){
-	setupModeButtons();
-	setupCircles();
-	reset();
+function init() {
+    setupModeButtons();
+    setupCircles();
+    reset();
 }
 
-function setupModeButtons(){
-    for(let i = 0; i < modeButtons.length; i++){
-        modeButtons[i].addEventListener("click", function(){
-			modeButtons[0].classList.remove("selected");
-			modeButtons[1].classList.remove("selected");
-			this.classList.add("selected");
-			this.textContent === "Beginner" ? numCircles = 3: numCircles = 6;
-			reset();
-		});
-	}
+function setupModeButtons() {
+    for (let i = 0; i < modeButtons.length; i++) {
+        modeButtons[i].addEventListener('click', function () {
+            modeButtons[0].classList.remove('selected');
+            modeButtons[1].classList.remove('selected');
+            this.classList.add('selected');
+            this.textContent === 'Beginner' ? (numCircles = 3) : (numCircles = 6);
+            reset();
+        });
+    }
 }
 
-function setupCircles(){
-	for(let i = 0; i < circles.length; i++){
-	//add click listeners to circles
-		circles[i].addEventListener("click", function(){
-			//grab color of clicked circles
-			let clickedColor = this.style.background;
-			//compare color to pickedColor
-			if(clickedColor === pickedColor){
-				messageDisplay.textContent = "Correct!";
-				resetButton.textContent = "Play Again?"
-				changeColors(clickedColor);
-				h1.style.background = clickedColor;
-			} else {
-				this.style.background = "#232323";
-				messageDisplay.textContent = "Try Again"
-			}
-		});
-	}
+function setupCircles() {
+    for (let i = 0; i < circles.length; i++) {
+        //add click listeners to circles
+        circles[i].addEventListener('click', function () {
+            //grab color of clicked circles
+            let clickedColor = this.style['background-color'];
+            //compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = 'Correct!';
+                resetButton.textContent = 'Play Again?';
+                changeColors(clickedColor);
+                h1.style['background-color'] = clickedColor;
+            } else {
+                this.style['background-color'] = '#232323';
+                messageDisplay.textContent = 'Try Again';
+            }
+        });
+    }
 }
 
-
-
-function reset(){
-	colors = generateRandomColors(numCircles);
-	//pick a new random color from array
-	pickedColor = pickColor();
-	//change colorDisplay to match picked Color
-	colorDisplay.textContent = pickedColor;
-	resetButton.textContent = "New Colors"
-	messageDisplay.textContent = "";
-	//change colors of circles
-	for(let i = 0; i < circles.length; i++){
-		if(colors[i]){
-			circles[i].style.display = "block"
-			circles[i].style.background = colors[i];
-		} else {
-			circles[i].style.display = "none";
-		}
-	}
-	h1.style.background = "steelblue";
+function reset() {
+    colors = generateRandomColors(numCircles);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match picked Color
+    colorDisplay.textContent = pickedColor;
+    resetButton.textContent = 'New Colors';
+    messageDisplay.textContent = '';
+    //change colors of circles
+    for (let i = 0; i < circles.length; i++) {
+        if (colors[i]) {
+            circles[i].style.display = 'block';
+            circles[i].style['background-color'] = colors[i];
+        } else {
+            circles[i].style.display = 'none';
+        }
+    }
+    h1.style['background-color'] = 'steelblue';
 }
 
-resetButton.addEventListener("click", function(){
-	reset();
-})
+resetButton.addEventListener('click', function () {
+    reset();
+});
 
-function changeColors(color){
-	//loop through all circles
-	for(let i = 0; i < circles.length; i++){
-		//change each color to match given color
-		circles[i].style.background = color;
-	}
+function changeColors(color) {
+    //loop through all circles
+    for (let i = 0; i < circles.length; i++) {
+        //change each color to match given color
+        circles[i].style['background-color'] = color;
+    }
 }
 
-function pickColor(){
-	let random = Math.floor(Math.random() * colors.length);
-	return colors[random];
+function pickColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
 
-function generateRandomColors(num){
-	//make an array
-	let arr = []
-	//repeat num times
-	for(let i = 0; i < num; i++){
-		//get random color and push into arr
-		arr.push(randomColor())
-	}
-	//return that array
-	return arr;
+function generateRandomColors(num) {
+    //make an array
+    let arr = [];
+    //repeat num times
+    for (let i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor());
+    }
+    //return that array
+    return arr;
 }
 
-function randomColor(){
-	//pick a "red" from 0 - 255
-	let r = Math.floor(Math.random() * 256);
-	//pick a "green" from  0 -255
-	let g = Math.floor(Math.random() * 256);
-	//pick a "blue" from  0 -255
-	let b = Math.floor(Math.random() * 256);
-	return "rgb(" + r + ", " + g + ", " + b + ")";
+function randomColor() {
+    //pick a "red" from 0 - 255
+    let r = Math.floor(Math.random() * 256);
+    //pick a "green" from  0 -255
+    let g = Math.floor(Math.random() * 256);
+    //pick a "blue" from  0 -255
+    let b = Math.floor(Math.random() * 256);
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
-
-
 
 init();
